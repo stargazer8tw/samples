f8c2cacbdab17e9d099824c2a9c1aa005d5bbf19
stargazer8tw <stargazer.taiwan@gmail.com>
2015-01-12 00:10:43 +0800

    change to bootstrap

--- ink_layout/js/main.js
+++ ink_layout/js/main.js
@@ -8,11 +8,63 @@ require.config({
         underscore: 'lib/underscore/underscore-min',
         backbone: 'lib/backbone/backbone-min',
         // none amd compliance
-        ink: 'lib/ink/ink.min',
+        bootstrap: 'lib/bootstrap/bootstrap.min',
         d3: 'lib/d3/d3.min',
         nvd3: 'lib/nvd3/nv.d3.min'
     },
     shim: {
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: '$.fn.popover'
+        },
+        'bootstrap/affix': {
+            deps: ['jquery'],
+            exports: '$.fn.affix'
+        },
+        'bootstrap/alert': {
+            deps: ['jquery'],
+            exports: '$.fn.alert'
+        },
+        'bootstrap/button': {
+            deps: ['jquery'],
+            exports: '$.fn.button'
+        },
+        'bootstrap/carousel': {
+            deps: ['jquery'],
+            exports: '$.fn.carousel'
+        },
+        'bootstrap/collapse': {
+            deps: ['jquery'],
+            exports: '$.fn.collapse'
+        },
+        'bootstrap/dropdown': {
+            deps: ['jquery'],
+            exports: '$.fn.dropdown'
+        },
+        'bootstrap/modal': {
+            deps: ['jquery'],
+            exports: '$.fn.modal'
+        },
+        'bootstrap/popover': {
+            deps: ['jquery'],
+            exports: '$.fn.popover'
+        },
+        'bootstrap/scrollspy': {
+            deps: ['jquery'],
+            exports: '$.fn.scrollspy'
+        },
+        'bootstrap/tab': {
+            deps: ['jquery'],
+            exports: '$.fn.tab'
+        },
+        'bootstrap/tooltip': {
+            deps: ['jquery'],
+            exports: '$.fn.tooltip'
+        },
+        'bootstrap/transition': {
+            deps: ['jquery'],
+            exports: '$.fn.transition'
+        },
         nvd3: {
             exports: 'nv',
             deps: ['d3.global']
@@ -26,8 +78,9 @@ define("d3.global", ["d3"], function(_) {
 });
 
 define([
+    'bootstrap',
     'router'
-], function (Router) {
+], function (Bootstrap, Router) {
     "use strict";
     Router.initialize();
 });
